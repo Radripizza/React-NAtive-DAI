34a3a268ec25cafe285232efd2182a642b3f57ab
Luca Radri <lucaradri04@gmail.com>
2022-08-16 08:41:49 -0300

    Ver 1.0
    Borre App.js
    Agregué una imagen (todavia no se hacer que funcione)
    Cree un nuevo componente

---
+++ View.js
@@ -0,0 +1,19 @@
+import { React} from 'react';
+import { Text, View } from 'react-native';
+
+const View = () => {
+  return (
+    <View style={{
+      flexDirection: "row",
+      height: 100,
+      padding: 20
+    }}>
+      <View style={{ backgroundColor: "blue", flex: 0.3 }}/>
+      <View style={{ backgroundColor: "red", flex: 0.5 }}/>
+      <Text>Momo en la Shell</Text>
+      <Image source={require('@React-NAtive-DAI/assets/momo.jpg')}></Image>
+    </View>
+  );
+}
+
+export default View;
