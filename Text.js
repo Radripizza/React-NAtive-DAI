f8034c9a4b45cb12f3fe7e7bd51b347deeaf0f28
Luca Radri <lucaradri04@gmail.com>
2022-08-16 09:00:32 -0300

    Ver 1.1
    Nuevo componente

---
+++ Text.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { Text, Stylesheet } from 'react-native';
+
+const Texto = () => {
+    const[titleText, setTitleText] = useState("Hey muy buenas a todos guapísimos");
+    const bodyText = "Aquí en un nuevo video para el canal.";
+};
